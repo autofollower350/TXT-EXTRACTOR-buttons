19959da186baf84f1bc0a0ac7b4a71b9a70a0f61
autofollower350 <followerauto350@gmail.com>
2026-04-29 20:25:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Direct variables - Simple aur Error-free
 API_ID = 28590286
 API_HASH = "6a68cc6b41219dc57b7a52914032f92f"
-BOT_TOKEN = "8021113365:AAGQjYo5uaSEsZbycIRXEw-W1NLpZ6Om2KQ"
+BOT_TOKEN = "7412939071:AAHZ-AnVuULdwuAlavNACdqMVyOI2lU3F2s"
 
 OWNER_ID = 6117445553 
 MONGO_URL = "mongodb+srv://Kailash979933:Lions98@cluster0.af4v4ge.mongodb.net/?appName=Cluster0"
