14843a0e8cff661b581a7e72d4abe44bd7bb0b43
autofollower350 <followerauto350@gmail.com>
2026-04-29 18:38:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
-# Direct variables - Simple aur Error-free
+# Configuration File
 API_ID = 28590286
 API_HASH = "6a68cc6b41219dc57b7a52914032f92f"
 BOT_TOKEN = "8021113365:AAGQjYo5uaSEsZbycIRXEw-W1NLpZ6Om2KQ"
 
-OWNER_ID = 6117445553 
+OWNER_ID = 6117445553
 MONGO_URL = "mongodb+srv://Kailash979933:Lions98@cluster0.af4v4ge.mongodb.net/?appName=Cluster0"
 
-# Agar aapko SUDO_USERS ya CHANNEL_ID chahiye, toh unhe niche uncomment karein:
-# SUDO_USERS = [5424499713] 
-# CHANNEL_ID = -1002656878420
+# Import errors se bachne ke liye inhe empty list ya string mein rakhein
+SUDO_USERS = []
+CHANNEL_ID = -1000000000000  # Agar channel ID nahi hai toh ye placeholder use karein
+PREMIUM_LOGS = ""
